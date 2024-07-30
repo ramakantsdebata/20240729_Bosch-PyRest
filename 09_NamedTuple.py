024a22dc518828934264e7f369d751fbc7af0cf5
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-07-30 17:07:36 +0530

    Day 02

--- 09_NamedTuple.py
+++ 09_NamedTuple.py
@@ -7,7 +7,7 @@ p2 = Point(30, 40)
 print(p1[0], p1[1])
 print(p1.x, p1.y)
 
-Student = namedtuple('Student_type', ['id', 'name', 'std'])
+Student = namedtuple('Student', ['id', 'name', 'std'])
 s1 = Student(1, "Manish", 5)
 print(s1.id, s1.name, s1.std)
 
