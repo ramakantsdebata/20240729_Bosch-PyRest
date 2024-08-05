c7e33496083b7c30829ee2f34bef7873fdeb0c72
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-08-05 15:35:59 +0530

    Day 06

--- 30_ClassOfStudents.py
+++ 30_ClassOfStudents.py
@@ -9,13 +9,41 @@ class Student:
     def __str__(self) -> str:
         return f"{self._roll}, {self._name}, {self._aggregate}"
     
+    @property
+    def aggregate(self):
+        return self._aggregate
+       
+    @aggregate.setter
+    def aggregate(self, value):
+        self._aggregate = value
+    
+
 class ClassRoom:
     # Create a collection of Students
     # 1. Enable the class to accept a collection in the initialiser
     # 2. Support adding/removing students to the classroom.
     # 3. Support printing the list of all students in the class
     # 4. Support accessing an individual student from the classroom, using indices (subscript operator)  --> clsRoom[5]
-    pass
+    def __init__(self, students = list()) -> None:
+        self._students = list(students)
+
+    def add(self, student: Student):
+        self._students.append(student)
+
+    def remove(self, student):
+        self._students.remove(student)
+
+    # def getCollection(self):
+    #     return self._students
+    
+
+    def __str__(self) -> str:
+        students = [str(student) for student in self._students]
+        return str(students)
+    
+    def __getitem__(self, idx: int):
+        return self._students[idx]
+    
 
 
 def Test():
@@ -31,10 +59,20 @@ def Test():
     print(clsRoom2)
     print(clsRoom2[3])
     clsRoom2[2].aggregate = 4.75
+    # print(dir(clsRoom2[2]))
 
     print(clsRoom2)
+    # print(clsRoom2.getCollection())
 
 
+    # print("\n\n")
+    # print(Student(1, "RR", 4.5).__dict__['roll'])
+    # for k, v in Student.__dict__.items():
+    #     print(k, v)
+
+    print("-"*40, "\n")
+    for student in clsRoom2:
+        print(student)
 
 if __name__ == '__main__':
     Test()
