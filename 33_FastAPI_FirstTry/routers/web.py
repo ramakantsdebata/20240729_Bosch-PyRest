b6434d3208ceac2687a1ce3767853e432cbbd7c4
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-08-12 18:28:10 +0530

    Dynamic HTML using JINJA

--- 33_FastAPI_FirstTry/routers/web.py
+++ 33_FastAPI_FirstTry/routers/web.py
@@ -2,24 +2,34 @@ import os
 from fastapi.responses import FileResponse
 from fastapi import APIRouter
 from starlette.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+from fastapi import Form, Depends
+from sqlmodel import Session
+
+from ..db import get_session
+from ..routers.cars import get_cars
 
 
 router = APIRouter()
 
+templates = Jinja2Templates(directory="33_FastAPI_FirstTry/templates")
 
 @router.get("/", response_class=HTMLResponse)
-def home():
-    return """
-    <html>
-        <head>
-            <title>Car Sharing Demo</title>
-        </head>
-        <body>
-            <h1>Welcome to the Car Sharing Service</h1>
-            <p>Here is some text for you</p>
-        </body>
-    </html>
-    """
+def home(request: Request):
+    return templates.TemplateResponse("home.html",
+                                      {"request": request})
+
+
+@router.post("/search", response_class=HTMLResponse)
+def search(*, 
+           size: str = Form(...), doors: int = Form(...),       # Form(...) --> no default value;  Form(2) --> default value of 2; type(...) --> class ellipsis
+           request: Request, 
+           session: Session = Depends(get_session)):
+    cars = get_cars(size=size, doors=doors, session=session)    # No session injected by FastAPI, as it's a regular function call. So, we provide for it.
+    return templates.TemplateResponse("search_results.html", 
+                                      {"request": request, "cars": cars})
+
 
 @router.get("/favicon.ico", include_in_schema=False)
 async def favicon():
